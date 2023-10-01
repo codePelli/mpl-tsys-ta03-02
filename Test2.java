ec22b9e0c3a14b862d14274e9b6ccb6b8a4635a0
Marc Pellicer <marcpelli3@gmail.com>
2023-10-01 12:10:46 +0200

    Tarea 02

---
+++ Test2.java
@@ -0,0 +1,19 @@
+public class Test2{
+
+	public static void main (String[] args){
+		//Creación de un objeto Integer usando el segundo constructor
+		Integer num1 = new Integer("125");
+
+		//Creación de un objeto Integer usando el primer constructor
+		Integer num2 = new Integer(20);
+
+		int n1 = num1.intValue();
+		int n2 = num2.intValue();
+		System.out.println("Suma de " + n1 + " y " + n2 + " vale " + (n1 + n2));
+		if (n1 + n2 > 130)
+			n1++;
+		else
+			n1--;
+		System.out.println(n1);
+	}
+}
